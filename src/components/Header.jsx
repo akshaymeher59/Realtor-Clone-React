f729b70d649921676e18b45b6700498fdfb6e023
Akshay <[meherakshay59@gmail.com]>
2022-11-17 12:55:40 +0530

    created sign in , sign up and forgot password pages oauth component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,16 +12,19 @@ const Header = () => {
                   return true;
             }
       }
-      return (
+    return (
+          
           <div className="bg-white border-b shadow-sm sticky top-0 z-50">
               <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
                   <div>
                       <img
                           src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+                        //   src="./logo.svg"
                           alt="logo"
                                     className="h-5 cursor-pointer"
                                     onClick={()=>navigate("/")}
-                      />
+                    />
+                  
                   </div>
                   <div>
                       <ul className="flex space-x-10">
